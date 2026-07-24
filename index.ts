0b243ced7135ffc7a5a36c84eeb2f1e4e16c20b9
Van Khai <hovankhaipro1@gmail.com>
2026-07-24 14:25:33 +0700

    Lấy ra danh sách các công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Oke 123")
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./configs/database";
+import Task from "./models/task.model";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  })
+  res.json(tasks);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
