9af035f292ece442c9d627f07731e103110d5875
Van Khai <hovankhaipro1@gmail.com>
2026-08-13 13:18:43 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import * as database from "./configs/database";
 import mainV1Routes from "./api/v1/routes/index.route";
 
@@ -9,6 +10,14 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200,
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
